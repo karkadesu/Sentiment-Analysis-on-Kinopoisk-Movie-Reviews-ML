c2429fd9b2d0a4dae742caf4f3f15e8d89966cf0
zhenya <hey.sin@ya.ru>
2023-04-04 23:39:05 +0300

    endpoints models

---
+++ backend/server/app/endpoints/urls.py
@@ -0,0 +1,17 @@
+from django.urls import re_path, include
+from rest_framework.routers import DefaultRouter
+
+from app.endpoints.views import EndpointViewSet
+from app.endpoints.views import MLAlgorithmViewSet
+from app.endpoints.views import MLAlgorithmStatusViewSet
+from app.endpoints.views import MLRequestViewSet
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"endpoints", EndpointViewSet, basename="endpoints")
+router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
+router.register(r"mlalgorithmstatuses", MLAlgorithmStatusViewSet, basename="mlalgorithmstatuses")
+router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+
+urlpatterns = [
+    re_path(r"^api/v1/", include(router.urls)),
+]
--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'app.endpoints',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
